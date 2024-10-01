d18ef7c366bd498d941fe36bf618959844094c75
Davi Limas <davi92limas@gmail.com>
2024-10-01 17:16:19 -0300

    Criando criptografia de senha

--- src/main/java/com/davi/usuario/controller/UsuarioController.java
+++ src/main/java/com/davi/usuario/controller/UsuarioController.java
@@ -6,7 +6,7 @@ import com.davi.usuario.infrastructure.repository.UsuarioRepository;
 import com.davi.usuario.infrastructure.repository.exceptions.ConflictException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +19,12 @@ public class UsuarioController {
 
     private final UsuarioService usuarioService;
     private final UsuarioRepository usuarioRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping
     public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
-        usuarioDTO.setSenha(bCryptPasswordEncoder.encode(usuarioDTO.getSenha()));
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
         return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
     }
 
